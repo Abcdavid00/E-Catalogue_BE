666fd67fa32878ac7f04daee05a8667062c0acdf
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-11-24 03:02:14 +0700

    Add sync and log config for database

--- UsersMS/src/config/mysql.service.ts
+++ UsersMS/src/config/mysql.service.ts
@@ -5,6 +5,8 @@ const DB_PORT = parseInt(process.env.DB_PORT, 10) || 3306;
 const DB_NAME = process.env.DB_NAME || 'users';
 const DB_USER = process.env.DB_USER || 'root';
 const DB_PASSWORD = process.env.DB_PASSWORD || 'password';
+const DB_SYNCHRONIZE = process.env.NODE_ENV === 'development';
+const DB_LOGGING = process.env.NODE_ENV === 'development';
 
 const mysqlConfig: TypeOrmModuleOptions = {
     type: 'mysql',
@@ -14,7 +16,9 @@ const mysqlConfig: TypeOrmModuleOptions = {
     password: DB_PASSWORD,
     database: DB_NAME,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true,
+    synchronize: DB_SYNCHRONIZE,
+    logging: DB_LOGGING,
+    migrations: [__dirname + '../migrations/**/*.{js,ts}'],
 };
 
 export default mysqlConfig;
