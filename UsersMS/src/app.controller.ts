d1afedfb204d79ad732e2e32a99cb6e485d9394e
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-11-26 04:14:29 +0700

    Add user-related API endpoints

--- UsersMS/src/app.controller.ts
+++ UsersMS/src/app.controller.ts
@@ -16,4 +16,39 @@ export class AppController {
 
     return this.appService.hi();
   }
+
+  @MessagePattern({ cmd: 'isUsernameAvailable' })
+  async isUsernameAvailable(username: string): Promise<boolean> {
+    return this.appService.isUsernameAvailable(username);
+  }
+
+  @MessagePattern({ cmd: 'isEmailAvailable' })
+  async isEmailAvailable(email: string): Promise<boolean> {
+    return this.appService.isEmailAvailable(email);
+  }
+
+  @MessagePattern({ cmd: 'createUser' })
+  async createUser(username: string, email: string, password: string): Promise<any> {
+    return this.appService.createUser(username, email, password);
+  }
+
+  @MessagePattern({ cmd: 'getUser' })
+  async getUser(id: number): Promise<any> {
+    return this.appService.getUser(id);
+  }
+
+  @MessagePattern({ cmd: 'findUserByUsername' })
+  async findUserByUsername(username: string): Promise<any> {
+    return this.appService.findUserByUsername(username);
+  }
+
+  @MessagePattern({ cmd: 'findUserByEmail' })
+  async findUserByEmail(email: string): Promise<any> {
+    return this.appService.findUserByEmail(email);
+  }
+
+  @MessagePattern({ cmd: 'signIn' })
+  async signIn(username: string, password: string): Promise<any> {
+    return this.appService.signIn(username, password);
+  }
 }
