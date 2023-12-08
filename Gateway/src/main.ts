16897571a028c53e96f6bb124b723ca07632a41a
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-12-08 15:52:07 +0700

    Fix Gateway

--- Gateway/src/main.ts
+++ Gateway/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import * as fs from 'fs';
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,7 +17,6 @@ async function bootstrap() {
     .build();
     
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  fs.writeFileSync("./Swagger-Spec.Json", JSON.stringify(Document));
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
