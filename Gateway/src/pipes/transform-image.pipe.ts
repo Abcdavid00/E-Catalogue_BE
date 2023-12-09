870758d24dcdab682fc59dbff13ef02557e72e5a
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-12-09 23:09:27 +0700

    Add FileServer module in Gateway

--- Gateway/src/pipes/transform-image.pipe.ts
+++ Gateway/src/pipes/transform-image.pipe.ts
@@ -1,54 +1,15 @@
 import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException, InternalServerErrorException } from '@nestjs/common';
 import * as convert from 'heic-convert';
 import * as sharp from 'sharp';
+import { ConvertToPNG } from 'src/file-server/image-converter';
 
 @Injectable()
 export class ToPNGPipe implements PipeTransform {
-  async transform(value: Express.Multer.File, metadata: ArgumentMetadata) {
+  async transform(value: Express.Multer.File, metadata: ArgumentMetadata): Promise<Express.Multer.File> {
     console.log("Pipe transform")
-    if (!value || !value.mimetype.startsWith('image/')) {
-      throw new BadRequestException(`Invalid file type uploaded (${value.mimetype})`);
-    }
-
-    let buffer: ArrayBuffer = value.buffer;
-    let mimetype: string = value.mimetype;
-    let size: number = value.size;
-    let originalname: string = value.originalname.split('.')[0];
-
-    switch (value.mimetype) {
-      case 'image/heic':
-        console.log("Converting HEIC to PNG")
-        buffer = await convert({
-          buffer: value.buffer,
-          format: 'PNG'
-        })
-        mimetype = 'image/png';
-        size = buffer.byteLength;
-        originalname += '.png';
-        break;
-      case 'image/jpeg':
-        console.log("Converting JPEG to PNG")
-        buffer = await sharp(value.buffer)
-          .png()
-          .toBuffer()
-        mimetype = 'image/png';
-        size = buffer.byteLength;
-        originalname += '.png';
-        break;
-      case 'image/png':
-        size = buffer.byteLength;
-        originalname += '.png';
-        break;
-      default:
-        throw new BadRequestException(`File type not supported (${value.mimetype})`);
-    }
-
-    if (mimetype !== 'image/png') {
-      throw new BadRequestException(`Image not supported (${value.mimetype})`);
-    }
-    
+    const res = await ConvertToPNG(value)
     console.log("Pipe transform done")
-    return { ...value, buffer, mimetype, size, originalname };
+    return res
   }
 }
 
