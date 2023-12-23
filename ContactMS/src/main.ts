a761f73e527d073427279736dd486ada70f3b593
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-12-23 15:30:48 +0700

    Add ContactMS connection in Gateway

--- ContactMS/src/main.ts
+++ ContactMS/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+const PORT = process.env.PORT;
+const HEALTHCHECK_PORT = process.env.HEALTHCHECK_PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: Number(PORT),
+  }})
+  await app.startAllMicroservices();
+  await app.listen(HEALTHCHECK_PORT);
 }
 bootstrap();
